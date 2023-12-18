07f31fd04fe2867133cc8c74e2bc7dfc4b413fdd
Daniil <dandombra1@gmail.com>
2023-12-18 20:29:06 +0300

    [refactoring]: <try to refact camera>

--- Assets/ThirdPartyAssets/MCR Creator/Assets/Scripts/Camera/Cam_Follow.cs
+++ Assets/ThirdPartyAssets/MCR Creator/Assets/Scripts/Camera/Cam_Follow.cs
@@ -2,98 +2,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class Cam_Follow : MonoBehaviour {
+public class Cam_Follow : MonoBehaviour
+{
+	[SerializeField] private CameraConfig _camConfig;
+	[SerializeField] private bool _isPaused = false;
+	[SerializeField] private bool _findTargetAutomaticly = false;
 
 	// The target we are following
-	public Transform 	target;
+	private Transform _target;
+
 	// The distance in the x-z plane to the target
-	public float 		distance = 10.0f;
+	private float _distance = 10.0f;
 	// the height we want the camera to be above the target
-	public float 		height = 5.0f;
-	public float 		rotationDamping;		
-	public float 		heightDamping;
+	private float _height = 5.0f;
+	private float _rotationDamping;
+	private float _heightDamping;
 
-	public bool 		b_Pause = false;	
+	// temp
+	void Start()
+	{
+		AutoFindTarget();
+		SetUp();
+    }
 
-	public bool 		b_Find_Target_Automatically = false;
+	private void SetUp()
+	{
+		_distance = _camConfig.Distance;
+		_height = _camConfig.Height;
+		_rotationDamping = _camConfig.RotationDamping;
+		_heightDamping = _camConfig.HeightDamping;
+	}
 
-	void Start(){
-		if (b_Find_Target_Automatically) {
-			GameObject[] arrCars = GameObject.FindGameObjectsWithTag("Car");							
+	private void AutoFindTarget()
+	{
+		if (!_findTargetAutomaticly)
+			return;
 
-			foreach (GameObject carFind in arrCars) {														// Put the car in the player order 1,2,3,4 on the array
-				if (carFind.GetComponent<CarController> ())
-					target = carFind.GetComponent<CarController> ().camTarget.transform;
+		GameObject[] arrCars = GameObject.FindGameObjectsWithTag("Car");
+
+		foreach (GameObject carFind in arrCars)
+		{                                                       // Put the car in the player order 1,2,3,4 on the array
+			if (carFind.TryGetComponent(out CarController carController))
+			{
+				_target = carController.camTarget.transform;
+				return;
 			}
+		}
+
+	}
+
+	// --> Init camera position when the scene starts
+	public void InitCamera(CarController car, bool b_Splitscreen)
+	{
+		_target = car.camTarget.transform;
+		if (b_Splitscreen)
+		{
+			Camera cam = GetComponent<Camera>();
+			cam.rect = new Rect(new Vector2(0, 0), new Vector2(.5f, 1));
 		}
 	}
 
-	void FixedUpdate () {
-		if (!b_Pause && target) {
+	void FixedUpdate()
+	{
+		if (!_isPaused && _target)
+		{
 			// Calculate the current rotation angles
-			float wantedRotationAngle = target.eulerAngles.y;
-			float wantedHeight = target.position.y + height;
+			float wantedRotationAngle = _target.eulerAngles.y;
+			float wantedHeight = _target.position.y + _height;
 
 			float currentRotationAngle = transform.eulerAngles.y;
 			float currentHeight = transform.position.y;
 
 
 			// Damp the rotation around the y-axis
-			currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+			currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, _rotationDamping * Time.deltaTime);
 
 			// Damp the height
-			currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, _heightDamping * Time.deltaTime);
 
 			// Convert the angle into a rotation
-			Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+			Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
 			// Set the position of the camera on the x-z plane to:
 			// distance meters behind the target
-			transform.position = target.position;
-			transform.position -= currentRotation * Vector3.forward * distance;
+			transform.position = _target.position;
+			transform.position -= currentRotation * Vector3.forward * _distance;
 
 			// Set the height of the camera
-			transform.position = new Vector3 (transform.position.x, currentHeight, transform.position.z);
+			transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 
 			// Always look at the target
-			transform.LookAt (target);
+			transform.LookAt(_target);
 		}
 	}
 
-// --> Init camera position when the scene starts
-	public void InitCamera(CarController car, bool b_Splitscreen){
-		target = car.camTarget.transform;
-		if (b_Splitscreen) {
-			Camera cam = GetComponent<Camera> ();
-			cam.rect = new Rect(new Vector2(0,0),new Vector2(.5f,1));
+	// --> Pause the camera in Pause Mode
+	public void Pause() =>
+		_isPaused = !_isPaused;
+	
 
+	// --> Init camera position when the scene starts
+	public void InitCameraHorizontal(CarController car, bool b_Splitscreen, string PlayerName)
+	{
+		_target = car.camTarget.transform;
+
+		if (b_Splitscreen && PlayerName == "P1")
+		{
+			Camera cam = GetComponent<Camera>();
+			cam.rect = new Rect(new Vector2(0, .5f), new Vector2(1, 1));
+		}
+		if (b_Splitscreen && PlayerName == "P2")
+		{
+			Camera cam = GetComponent<Camera>();
+			cam.rect = new Rect(new Vector2(0, -.5f), new Vector2(1, 1));
 		}
 	}
-
-// --> Pause the camera in Pause Mode
-	public void Pause(){
-		if (!b_Pause) {
-			b_Pause = true;
-		} else {
-			b_Pause = false;
-		}
-	}
-
-    // --> Init camera position when the scene starts
-    public void InitCameraHorizontal(CarController car, bool b_Splitscreen,string PlayerName)
-    {
-        target = car.camTarget.transform;
-
-        if (b_Splitscreen && PlayerName == "P1")
-        {
-            Camera cam = GetComponent<Camera>();
-            cam.rect = new Rect(new Vector2(0, .5f), new Vector2(1, 1));
-        }
-        if (b_Splitscreen && PlayerName == "P2")
-        {
-            Camera cam = GetComponent<Camera>();
-            cam.rect = new Rect(new Vector2(0, -.5f), new Vector2(1, 1));
-        }
-    }
 }
