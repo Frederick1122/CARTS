faefa8e54a6873763f1ae845cf45d705286a5103
Fyodor Zaletov <zaletovfyodor@gmail.com>
2023-12-31 14:29:32 +0300

    Added core scripts, fonts, scenes and UI-elements

---
+++ Assets/Scripts/Core/FSM/Fsm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.FSM
+{
+    public class Fsm
+    {
+        private FsmState _currentState;
+        private Dictionary<Type, FsmState> _states = new Dictionary<Type, FsmState>();
+
+        public Fsm()
+        {
+            Init();
+        }
+        
+        protected virtual void Init() { }
+        
+        public void SetState<T>() where T : FsmState
+        {
+            var type = typeof(T);
+            
+            if (_currentState.GetType() == type)
+                return;
+
+            if (_states.TryGetValue(type, out var newState))
+            {
+                _currentState.Exit();
+                _currentState = newState;
+                _currentState.Enter();
+            }
+            else
+            {
+                Debug.LogAssertion($"{GetType()} not found state {type}");
+            }
+        }
+
+        public void Update()
+        {
+            _currentState?.Update();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/FSM/FsmManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Base;
+
+namespace Core.FSM
+{
+    public class FsmManager : Singleton<FsmManager>
+    {
+        private Dictionary<Type, Fsm> _currentFsms = new Dictionary<Type, Fsm>();
+        
+        public Fsm TryGetFsm<T>() where T : Fsm
+        {
+            AddNewFsm<Fsm>();
+            return _currentFsms.TryGetValue(typeof(T), out var fsm) ? fsm : null;
+        }
+
+        public void AddNewFsm<T>() where T : Fsm, new()
+        {
+            if (_currentFsms.ContainsKey(typeof(T)))
+                return;
+            
+            _currentFsms.Add(typeof(T), new T()); 
+        }
+
+        public void RemoveFsm<T>() where T : Fsm
+        {
+            _currentFsms.Remove(typeof(T));
+        }
+    }
+}
