30c63a6e74d03022a916890203e3338939b29cfb
Fyodor Zaletov <zaletovfyodor@gmail.com>
2024-01-30 13:00:12 +0300

    [add] add LapRaceEndWindow (temp commit)

---
+++ Assets/Scripts/UI/Windows/Race/RaceUI/LapRaceEndWindow/LapRaceEndWindowController.cs
@@ -0,0 +1,14 @@
+﻿namespace UI.Windows.LapRace.LapRaceEndWindow
+{
+    public class LapRaceEndWindowController : UIController<LapRaceEndWindowView, LapRaceEndWindowModel>
+    {
+        private LapRaceEndWindowModel _lapRaceEndWindowModel;
+            
+        
+        
+        protected override LapRaceEndWindowModel GetViewData()
+        {
+            return _lapRaceEndWindowModel;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Windows/Race/RaceUI/LapRaceEndWindow/LapRaceEndWindowView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Windows.LapRace.LapRaceEndWindow
+{
+    public class LapRaceEndWindowView : UIView<LapRaceEndWindowModel>
+    {
+        public event Action OnGoToMainMenuAction = delegate {  };
+        
+        [SerializeField] private TMP_Text _currentPosition;
+        [SerializeField] private TMP_Text _currentTime;
+        [SerializeField] private Button _goToMainMenu;
+
+        public override void Init(LapRaceEndWindowModel uiModel)
+        {
+            base.Init(uiModel);
+            _goToMainMenu.onClick.AddListener(OnGoToMainMenuAction.Invoke); 
+        }
+
+        private void OnDestroy()
+        {
+            if (_goToMainMenu != null)
+                _goToMainMenu.onClick.RemoveListener(OnGoToMainMenuAction.Invoke);
+        }
+
+        public override void UpdateView(LapRaceEndWindowModel uiModel)
+        {
+            _currentPosition.text = $"{uiModel.currentPosition} / {uiModel.maxPosition}";
+            _currentTime.text = String.Format("{0:00}:{1:00}.{2:00}", uiModel.passTime.Minutes, uiModel.passTime.Seconds, 
+                uiModel.passTime.Milliseconds / 10);
+            
+            base.UpdateView(uiModel);
+        }
+    }
+
+    public class LapRaceEndWindowModel : UIModel
+    {
+        public int maxPosition;
+        public int currentPosition;
+        public TimeSpan passTime;
+    }
+}
