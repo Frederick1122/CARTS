844865c2ec571b47310860890c45f04f73972f6f
Frederick1122 <zaletovfyodor@gmail.com>
2024-04-20 23:39:26 +0300

    [add] localization part 1

---
+++ Assets/Scripts/Managers/LocalizationManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Base;
+using Knot.Localization;
+
+namespace Managers
+{
+    public class LocalizationManager : Singleton<LocalizationManager>
+    {
+        private const string RUSSIAN = "Russian";
+        private const string ENGLISH = "English";
+
+        public void SetLocalization(LocalizationLanguage language)
+        {
+            switch (language)
+            {
+                case LocalizationLanguage.Russian:
+                    //LeanLocalization.SetCurrentLanguageAll(RUSSIAN);
+                    break;
+                case LocalizationLanguage.English:
+                    //LeanLocalization.SetCurrentLanguageAll(ENGLISH);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(language), language, null);
+            }
+        }
+    }
+
+    public enum LocalizationLanguage
+    {
+        Russian,
+        English
+    }
+}
