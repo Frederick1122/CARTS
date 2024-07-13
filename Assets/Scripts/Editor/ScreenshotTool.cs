fa1fbe3b84864c58cb64fc353c3aa96cb379093a
Frederick1122 <zaletovfyodor@gmail.com>
2024-07-13 21:59:03 +0300

    [add] screenshot_tool

---
+++ Assets/Scripts/Editor/ScreenshotTool.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace Core
+{
+    public class ScreenshotTool : EditorWindow
+    {
+        [MenuItem("Tools/Screenshot Tool")]
+        public static void ShowWindow()
+        {
+            GetWindow<ScreenshotTool>("Screenshot Tool");
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Take Screenshot"))
+            {
+                TakeScreenshot();
+            }
+        }
+        
+        private void TakeScreenshot()
+        {
+            string directoryPath = Path.Combine(Application.dataPath, "Screenshots");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string fileName = "Screenshot_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            ScreenCapture.CaptureScreenshot(filePath);
+            Debug.Log("Screenshot saved to: " + filePath);
+        }
+    }
+}
+
